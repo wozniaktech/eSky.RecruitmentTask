682d1c13ba12fd4ce724b76e896d0ebab7a03ab5
DESKTOP-51RD2LT\piotr <piotr.b.wozniak@gmail.com>
2024-01-21 14:22:05 +0100

    Added AuthorTest

---
+++ eSky.RecruitmentTask.Test/Services/AuthorServiceTest.cs
@@ -0,0 +1,60 @@
+﻿using eSky.RecruitmentTask.Config;
+using eSky.RecruitmentTask.Controllers;
+using eSky.RecruitmentTask.Services;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Moq;
+using System.Net;
+
+namespace eSky.RecruitmentTask.Test.Services
+{
+    public class AuthorServiceTest
+    {
+        int NumberOfAuthors = 3;
+        Mock<IHttpService> HttpService;
+        EndpointConfig Config;
+        IOptionsMonitor<EndpointConfig>? Monitor;
+        Mock<ILogger<AuthorService>> Logger;
+
+        public AuthorServiceTest()
+        {
+            Logger = new Mock<ILogger<AuthorService>>();
+            HttpService = new Mock<IHttpService>();
+
+            Config = new EndpointConfig 
+            {
+                Authors = "http://authors.com",
+                Poems = "http://poems.com"
+            };
+            Monitor = Mock.Of<IOptionsMonitor<EndpointConfig>>(_ => _.CurrentValue == Config);
+
+            
+
+        }
+
+        [Fact]
+        public async Task GetAuthors_ShoudReturn_IEnumerableAuthors()
+        {
+            //arrange
+            IEnumerable<string> result1 = new List<string>
+            {
+                "Stefan","Roman","Tadek"
+            };
+
+            //Task<HttpResponseMessage> response = new Task<HttpResponseMessage>( HttpStatusCode.OK)
+            //{
+                
+            //};
+
+            AuthorService sut = new AuthorService(HttpService.Object, Monitor, Logger.Object);
+            //HttpService.Setup(_ => _.GetAsync(Monitor.CurrentValue.Authors)).Returns(response);
+            
+
+            //act
+            var result = await sut.GetAuthors(NumberOfAuthors);
+
+            //var optionsMonitor = new OptionsMonitor(options);
+
+        }
+    }
+}
